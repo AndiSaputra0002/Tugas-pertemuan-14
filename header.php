ad583d07986b51cdb59a7afc7bae552bf6431094
AndiSaputra0002 <65064135+AndiSaputra0002@users.noreply.github.com>
2020-07-04 18:15:05 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $title;?></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Data Barang</h1>
+		<div class="main">
